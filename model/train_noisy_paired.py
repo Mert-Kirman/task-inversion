a7403c8d04b5dc56678280dbaa65374c41aaa6d6
Mert Kirman <116290807+Mert-Kirman@users.noreply.github.com>
2025-10-29 01:35:30 +0300

    Refactor training code. Add necessary files for the training script.

--- model/train_noisy_paired.py
+++ model/train_noisy_paired.py
@@ -7,41 +7,84 @@ import numpy as np
 import math
 import importlib
 import validate_model
-import model_noisy_paired as dual_enc_dec_cnmp
+import model_mock_perfectly_paired as dual_enc_dec_cnmp
 import utils
-importlib.reload(dual_enc_dec_cnmp)
-importlib.reload(utils)
 import os
 from tqdm import tqdm
 import sys
+from torch.optim.lr_scheduler import LambdaLR
+import time
 
-folders = ["1/","2/", "3/", "4/", "5/"]
+def train(model, optimizer, scheduler, EPOCHS, unpaired_traj=True):
 
-for folder in folders:
-    Y1_Paired = torch.tensor(np.load("data/noisy_paired/forward_noisy_paired_data.npy"), dtype=torch.float32)
-    Y2_Paired = torch.tensor(np.load("data/noisy_paired/inverse_noisy_paired_data.npy"), dtype=torch.float32)
+    os.makedirs(f'logs/{data_type}/run_{run_id}/', exist_ok=True)
+    sys.stdout = open(f'logs/{data_type}/run_{run_id}/train_log.txt', 'w')
 
-    Y1_Extra = torch.tensor(np.load("data/" + folder + "forward_extra_data.npy"), dtype=torch.float32)
-    Y2_Extra = torch.tensor(np.load("data/" + folder + "inverse_extra_data.npy"), dtype=torch.float32)
+    errors = []
+    losses = []
+
+    unpaired_traj = True 
+    
+    for i in tqdm(range(EPOCHS)):
+
+        extra_pass = False
+        if unpaired_traj:
+            p = np.random.random_sample()
+            if p < 0.20:
+                extra_pass = True
+
+        obs, params, mask, x_tar, y_tar_f, y_tar_i, extra_pass = dual_enc_dec_cnmp.get_training_sample(extra_pass, valid_inverses, demo_data, OBS_MAX, d_N, d_x, d_y1, d_y2, d_param, time_len)
+        optimizer.zero_grad()
+        output, L_F, L_I, extra_pass = model(obs, params, mask, x_tar, extra_pass)
+        
+        loss = dual_enc_dec_cnmp.loss(output, y_tar_f, y_tar_i, d_y1, d_y2, d_param, L_F.squeeze(1), L_I.squeeze(1), extra_pass)
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 5.0)
+
+
+        optimizer.step()
+        scheduler.step()
+
+        if i > 35000 and i % 100 == 0:
+            epoch_val_error = validate_model.val_only_extra(model, validation_indices, i, demo_data, 
+                                                            d_x, d_y1, d_y2)
+            errors.append(epoch_val_error)
+            losses.append(loss.item())
+
+            if min(errors) == errors[-1]:
+                tqdm.write(f"Run ID: {run_id}, Saved model epoch {i}, Train loss: {loss.item():6f}, Validation error: {epoch_val_error:6f}")
+                torch.save(model.state_dict(), f'{save_folder}/run_{run_id}/perfectly_paired.pth')
+
+    return errors, losses
+
+
+if __name__ == "__main__":
+    data_type = "perfect_paired/sin"
+    data_folder = f"mock_data/{data_type}"
+
+    # Load trajectory(sensorimotor) data
+    Y1_Paired = torch.load(f"{data_folder}/forward_data.pt", weights_only=True)
+    Y2_Paired = torch.load(f"{data_folder}/inverse_data.pt", weights_only=True)
+
+    Y1_Extra = torch.load(f"{data_folder}/forward_extra_data.pt", weights_only=True)
+    Y2_Extra = torch.load(f"{data_folder}/inverse_extra_data.pt", weights_only=True)
 
     Y1 = torch.cat((Y1_Paired, Y1_Extra), dim=0)
     Y2 = torch.cat((Y2_Paired, Y2_Extra), dim=0)
 
-    CI = torch.tensor(np.load("data/" + folder + "noisy_paired/context_pretrained_features_lower_train_dim75.npy"), dtype=torch.float32)
-    C1_Paired = torch.tensor(np.load("data/noisy_paired/context_noisy_paired_data.npy"), dtype=torch.float32)[:, [0,1]]
-    C1_Extra = torch.tensor(np.load("data/" + folder + "context_extra_data.npy"), dtype=torch.float32)[:, [0, 1]]
+    # Load context data (Task Parameters)
+    C1_Paired = torch.load(f"{data_folder}/context_paired_data.pt", weights_only=True)
+    C1_Extra = torch.load(f"{data_folder}/context_extra_data.pt", weights_only=True)
 
-    C1 = torch.cat((C1_Paired, C1_Extra), dim=0)
-    C1 = (C1 - (-0.15)) / (0.30)
-    C = torch.cat((C1, CI), dim=-1)
+    C = torch.cat((C1_Paired, C1_Extra), dim=0)
 
-
+    # Normalize Y1 and Y2 together
     for dim in range(Y1.shape[2]):
         min_dim = torch.minimum(Y1[:, :, dim].min(), Y2[:, :, dim].min())
         max_dim = torch.maximum(Y1[:, :, dim].max(), Y2[:, :, dim].max())
         Y1[:, :, dim] = (Y1[:, :, dim] - min_dim) / (max_dim - min_dim)
         Y2[:, :, dim] = (Y2[:, :, dim] - min_dim) / (max_dim - min_dim)
-        #print(f"Dimension {dim}: min={min_dim}, max={max_dim}")
 
     num_demo = Y1.shape[0]
     time_len = Y1.shape[1]
@@ -49,7 +92,7 @@ for folder in folders:
     X1 = torch.linspace(0, 1, time_len).repeat(num_demo, 1).reshape(num_demo, -1, 1)
     X2 = torch.linspace(0, 1, time_len).repeat(num_demo, 1).reshape(num_demo, -1, 1)
 
-    valid_inverses = [True for i in range(60)] + [False for i in range(20)]
+    valid_inverses = [True for i in range(Y1_Paired.shape[0])] + [False for i in range(Y1_Extra.shape[0])]
 
     d_x = 1
     d_param = C.shape[1]
@@ -60,79 +103,22 @@ for folder in folders:
     d_N = num_demo
 
     # last 20 traj are extra
-    # poke: 60-63
-    # push: 64-72
-    # pick: 73-80
     validation_indices = [69, 71, 74, 76]     
 
     demo_data = [X1, X2, Y1, Y2, C]
 
+    save_folder = f"save/{data_type}"
+    run_id = time.time()
+    os.makedirs(f'{save_folder}/run_{run_id}', exist_ok=True)
 
-    importlib.reload(dual_enc_dec_cnmp)
-    importlib.reload(utils)
+    errors = []
+    losses = []
+    errors_with_latent = []
 
-    def train(model, optimizer, scheduler, EPOCHS, run_id, unpaired_traj=True):
-        
-        os.makedirs(f'logs/{folder}run{run_id}', exist_ok=True)
-        sys.stdout = open(f'logs/{folder}run{run_id}/noisy_train_log.txt', 'w')
+    EPOCHS = 60_000
+    learning_rate = 1e-3
+    model = dual_enc_dec_cnmp.DualEncoderDecoder(d_x, d_y1, d_y2, d_param)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-2)
+    scheduler = LambdaLR(optimizer, lr_lambda=lambda epoch: 1 if epoch < 40_000 else 5e-1)
 
-        errors = []
-        losses = []
-
-        unpaired_traj = True 
-        
-        for i in tqdm(range(EPOCHS)):
-
-            extra_pass = False
-            if unpaired_traj:
-                p = np.random.random_sample()
-                if p < 0.20:
-                    extra_pass = True
-
-            obs, params, mask, x_tar, y_tar_f, y_tar_i, extra_pass = dual_enc_dec_cnmp.get_training_sample(extra_pass, validation_indices, 
-                                                                    valid_inverses, demo_data, OBS_MAX, d_N, d_x, d_y1, d_y2, d_param, time_len)
-            optimizer.zero_grad()
-            output, L_F, L_I, extra_pass = model(obs, params, mask, x_tar, extra_pass)
-            
-            loss = dual_enc_dec_cnmp.loss(output, y_tar_f, y_tar_i, d_y1, d_y2, d_param, L_F.squeeze(1), L_I.squeeze(1), extra_pass)
-            loss.backward()
-
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 5.0)
-
-
-            optimizer.step()
-            scheduler.step()
-
-            if i > 35000 and i % 100 == 0:
-                epoch_val_error = validate_model.val_only_extra(model, validation_indices, i, demo_data, 
-                                                                d_x, d_y1, d_y2)
-                errors.append(epoch_val_error)
-                losses.append(loss.item())
-
-                if min(errors) == errors[-1]:
-                    tqdm.write(f"Run ID: {run_id}, Saved model epoch {i}, Train loss: {loss.item():6f}, Validation error: {epoch_val_error:6f}")
-                #tqdm.write(f"Run ID: {run_id}, Saved model epoch {i}, Train loss: {loss.item():6f}")
-                    #torch.save(model.state_dict(), f'save/{folder}run{run_id}/perfect_paired_{i}.pth')
-                    torch.save(model.state_dict(), f'save/{folder}run{run_id}/noisy_paired.pth')
-                    
-        # save last model
-        #torch.save(model.state_dict(), f'save/{folder}run{run_id}/perfect_paired_final_{i}.pth')
-    
-        return errors, losses
-    
-    
-    from torch.optim.lr_scheduler import LambdaLR
-
-    for run_id in range(1, 3):
-        os.makedirs(f'save/{folder}run{run_id}', exist_ok=True)
-        errors = []
-        losses = []
-        errors_with_latent = []
-
-        EPOCHS = 80_000
-        learning_rate = 1e-3
-        model = dual_enc_dec_cnmp.DualEncoderDecoder(d_x, d_y1, d_y2, d_param)
-        optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-2)
-        scheduler = LambdaLR(optimizer, lr_lambda=lambda epoch: 1 if epoch < 40_000 else 5e-1)
-
-        train(model, optimizer, scheduler, EPOCHS, run_id, unpaired_traj=True)
+    train(model, optimizer, scheduler, EPOCHS, unpaired_traj=True)
---
+++ model/validate_model.py
@@ -0,0 +1,143 @@
+import numpy as np
+import utils
+import model_predict
+import torch
+
+def val(model, validation_indices, epoch_count, demo_data, d_x, d_y1, d_y2, d_param, time_len=200):
+
+    model.eval()
+    [_, __, Y1, Y2, C] = demo_data
+    error = 0
+    plot_id = np.random.randint(0, len(validation_indices))
+    for validation_idx in validation_indices:
+        time = np.linspace(0, 1, time_len)
+        # permute time
+        idx = np.random.permutation(time_len)
+        
+        #idx = idx[:3]
+        idx = [0]
+
+        time = [time[i] for i in idx]
+        f_condition_points = [[t, Y1[validation_idx, i:i+1]] for t,i in zip(time, idx)]
+        i_condition_points = [[t, Y2[validation_idx, i:i+1]] for t,i in zip(time, idx)]
+
+        context = C[validation_idx]
+
+        fi_means, fi_stds = model_predict.predict_inverse(model, time_len, context,
+                                                          f_condition_points, d_x, d_y1, d_y2)
+        ff_means, ff_stds = model_predict.predict_forward_forward(model, time_len, context,
+                                                                   f_condition_points, d_x, d_y1, d_y2)
+        ii_means, ii_stds = model_predict.predict_inverse_inverse(model, time_len, context, 
+                                                                  i_condition_points, d_x, d_y1, d_y2)
+        if_means, if_stds = model_predict.predict_forward(model, time_len, context,
+                                                          i_condition_points, d_x, d_y1, d_y2)
+        
+        if epoch_count % 200_000 == 0 and validation_idx == validation_indices[plot_id]:
+            error += utils.validate_model(fi_means, fi_stds, validation_idx, demo_data, time_len, f_condition_points, epoch_count,d_y1, d_y2, forward=False, plot=True)
+            error += utils.validate_model(ff_means, ff_stds, validation_idx, demo_data, time_len, f_condition_points, epoch_count, d_y1, d_y2, forward=True, plot=True)
+        else:
+            error += utils.validate_model(ff_means, ff_stds, validation_idx, demo_data, time_len, f_condition_points, epoch_count, d_y1, d_y2,forward=True, plot=False)
+            error += utils.validate_model(fi_means, fi_stds, validation_idx, demo_data, time_len, f_condition_points, epoch_count,d_y1, d_y2, forward=False, plot=False)
+
+        error += utils.validate_model(ii_means, ii_stds, validation_idx, demo_data, time_len, i_condition_points, epoch_count,d_y1, d_y2, forward=False, plot=False)
+        error += utils.validate_model(if_means, if_stds, validation_idx, demo_data, time_len, i_condition_points, epoch_count,d_y1, d_y2, forward=True, plot=False)
+    
+    model.train()
+    return error / (len(validation_indices) * 4)  # average error over all validation indices and both forward and inverse predictions
+
+
+import numpy as np
+import utils
+import model_predict
+
+def val_only_extra(model, validation_indices, epoch_count, demo_data, d_x, d_y1, d_y2, 
+                   time_len=200, plot_freq=200_000):
+
+    model.eval()
+
+    [_, __, Y1, ___, C] = demo_data
+    error = 0
+    plot_id = np.random.randint(0, len(validation_indices))
+    for validation_idx in validation_indices:
+
+        time = np.linspace(0, 1, time_len)
+        # permute time
+        idx = np.random.permutation(time_len)
+        
+        #idx = idx[:3]
+        idx = [0]
+
+        time = [time[i] for i in idx]
+        f_condition_points = [[t, Y1[validation_idx, i:i+1]] for t,i in zip(time, idx)]
+        
+        context = C[validation_idx]
+
+        ff_means, ff_stds = model_predict.predict_forward_forward(model, time_len, context,
+                                                                   f_condition_points, d_x, d_y1, d_y2)
+        
+        if epoch_count % plot_freq == 0 and validation_idx == validation_indices[plot_id]:
+            error += utils.validate_model(ff_means, ff_stds, validation_idx, demo_data, time_len, f_condition_points, epoch_count, d_y1, d_y2, forward=True, plot=True)
+        else:
+            error += utils.validate_model(ff_means, ff_stds, validation_idx, demo_data, time_len, f_condition_points, epoch_count, d_y1, d_y2,forward=True, plot=False)
+
+    model.train()
+    
+    return error / (len(validation_indices))  # average error over all validation indices and both forward and inverse predictions
+
+
+
+def val_features(model, validation_indices, corresponding_indices, demo_data):
+
+    model.eval()
+    [_, __, _, _, C] = demo_data
+    error = 0
+    for i in range(len(validation_indices)):
+
+        validation_idx = validation_indices[i]
+
+        # val features
+        val_C = C[validation_idx, 1:]
+        # corresponding features
+        corresponding_C = C[corresponding_indices[i], 1:]
+
+        val_embedding = model.mlp(val_C)
+        corresponding_embedding = model.mlp(corresponding_C)
+
+        error += torch.nn.functional.mse_loss(val_embedding, corresponding_embedding)
+
+    model.train()
+    
+    return error / (len(validation_indices))  # average error over all validation indices and both forward and inverse predictions
+
+
+
+def val_extrapolation(model, validation_indices, corresponding_indices, demo_data,d_x, d_y1, d_y2, time_len=200):
+
+    model.eval()
+    [_, __, Y1, Y2, C] = demo_data
+    error = 0
+    for i in range(len(validation_indices)):
+
+        validation_idx = validation_indices[i]
+
+        # val features
+        context = C[validation_idx, :]
+
+        time = np.linspace(0, 1, time_len)
+        # permute time
+        idx = np.random.permutation(time_len)
+        
+        #idx = idx[:3]
+        idx = [0]
+
+        time = [time[i] for i in idx]
+        f_condition_points = [[t, Y1[validation_idx, i:i+1]] for t,i in zip(time, idx)]
+
+        fi_means, fi_stds = model_predict.predict_inverse(model, time_len, context,
+                                                                   f_condition_points, d_x, d_y1, d_y2)
+        
+        error += torch.nn.functional.mse_loss(fi_means, Y2[corresponding_indices[i], :, :])
+        
+    model.train()
+    
+    return error / (len(validation_indices))  # average error over all validation indices and both forward and inverse predictions
