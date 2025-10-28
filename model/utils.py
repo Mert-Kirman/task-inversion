8025c4b1c1e4c6f51740df86d5ba1b6f3a553e5d
Mert Kirman <116290807+Mert-Kirman@users.noreply.github.com>
2025-10-29 01:27:25 +0300

    Modify generate_demonstrations() function to be suitable for mock data creation

--- model/utils.py
+++ model/utils.py
@@ -42,74 +42,87 @@ def generate_demonstrations(num_demo, time_len = 200, params = None, plot_title 
     x = torch.linspace(0, 1, time_len)
     times = torch.zeros((2*(num_demo), time_len, 1))
     times[:] = x.reshape((1, time_len, 1))
-    values = torch.zeros((2*(num_demo), time_len, 1))
 
-    #frequencies = np.linspace(0.75,1.5,num_demo//2)  # Example frequencies
-    f_frequencies = [1, 1.5, 2]  # Example frequencies
-    i_frequencies = [1, 1.5, 2]  # Example frequencies
-    #frequencies = np.linspace(1,2,num_demo)
-    #amplitudes = [1.1, 1]  # Example amplitudes
-    amplitudes = torch.linspace(0.1,0.5,num_demo)
+    num_paired = int(num_demo * 0.75)
+    num_extra = int(num_demo * 0.25)
+
+    values = torch.zeros((2*num_paired, time_len, 1))
+    extra_values = torch.zeros((2*num_extra, time_len, 1))
     
+    context_params_paired = torch.zeros((num_paired, 1))
+    context_params_extra = torch.ones((num_extra, 1))
+
+    f_frequencies = [1, 1.5, 2]  # Example frequencies
+    amplitudes = torch.linspace(0.1,0.5,num_paired)
+    extra_amplitudes = torch.linspace(1.1,1.5,num_extra)
+
     phases = [0]  # Example phases
         
-    for d in range(num_demo):
-
+    for d in range(num_paired):
         f_dist1 = sinx(f_frequencies[0], amplitudes[d % len(amplitudes)], phases[d % len(phases)])
-        #f_dist2 = sinx(f_frequencies[1], 0.3*amplitudes[d % len(amplitudes)], phases[d % len(phases)])
-        #f_dist3 = sinx(f_frequencies[2], 0.5*amplitudes[d % len(amplitudes)], phases[d % len(phases)])
-
-        #i_dist1 = sinx(i_frequencies[0], 0.2*amplitudes[d % len(amplitudes)], phases[d % len(phases)])
-        #i_dist2 = sinx(i_frequencies[1], amplitudes[d % len(amplitudes)], phases[d % len(phases)])
-        #i_dist3 = sinx(i_frequencies[2], 0.7*amplitudes[d % len(amplitudes)], phases[d % len(phases)])
-
 
         for i in range(time_len):
-            values[d, i] = f_dist1(x[i]*0.5) #+ f_dist2(x[i]*0.38) + f_dist3(x[i]*0.38)
-        # reverse array
-        
+            values[d, i] = f_dist1(x[i]*0.5)     
 
         for i in range(time_len):
-           values[d+num_demo, i] = -1*(f_dist1(x[i]*0.5)) #+ f_dist2(x[i]*0.6+0.4) + f_dist3(x[i]*0.6+0.4)
+           values[d+num_paired, i] = -1*(f_dist1(x[i]*0.5))
 
-
-        #values[d+num_demo] = -1 * values[d]
-        # normalize between -1 , 1
-        #values[d] = (values[d] - np.min(values[d])) / (np.max(values[d]) - np.min(values[d])) * 2 - 1
-        #values[d+num_demo] = (values[d+num_demo] - np.min(values[d+num_demo])) / (np.max(values[d+num_demo]) - np.min(values[d+num_demo])) * 2 - 1
+        # Plot demonstrations
         plt.plot(times[d], values[d], color="black", alpha=0.5)
-        plt.plot(times[d], values[d+num_demo], color="black", alpha=0.5)
-    
+        plt.plot(times[d], values[d+num_paired], color="black", alpha=0.5)
+
+    for d in range(num_extra):
+        f_dist2 = sinx(f_frequencies[0], extra_amplitudes[d % len(extra_amplitudes)], phases[d % len(phases)])
+
+        for i in range(time_len):
+            extra_values[d, i] = f_dist2(x[i]*0.5)        
+
+        for i in range(time_len):
+           extra_values[d+num_extra, i] = -1*(f_dist2(x[i]*0.5))
+
+        # Plot demonstrations
+        plt.plot(times[d], extra_values[d], color="black", alpha=0.5)
+        plt.plot(times[d], extra_values[d+num_extra], color="black", alpha=0.5)
+
     plt.title(plot_title + ' Demonstrations')
     plt.ylabel('Y')
     plt.xlabel('time (t)')
-    #fig("../figs/TrainingDemonstrations.png")
     plt.show()
-    
-    forward = values[:num_demo]
-    inverse = values[num_demo:]
-    # num_demo is the half of the number of demonstrations
-    Y1 = forward.reshape(num_demo, time_len, 1)
-    Y2 = inverse.reshape(num_demo, time_len, 1)
+
+    forward = values[:num_paired]
+    inverse = values[num_paired:]
+    extra_forward = extra_values[:num_extra]
+    extra_inverse = extra_values[num_extra:]
+
+    Y1 = forward.reshape(num_paired, time_len, 1)
+    Y2 = inverse.reshape(num_paired, time_len, 1)
     X1 = times[:1]
     X2 = times[:1]
 
+    Y1_extra = extra_forward.reshape(num_extra, time_len, 1)
+    Y2_extra = extra_inverse.reshape(num_extra, time_len, 1)
+
     plt.title("Forward Trajectories")
-    for i in range(num_demo):
+    for i in range(num_paired):
         plt.plot(times[i], forward[i], color = "blue")
+
+    for i in range(num_extra):
+        plt.plot(times[i], extra_forward[i], color = "blue")
+    
     plt.grid(alpha=0.3)
-    #plt.ylim(-0.3, 0.3)
     plt.show()
 
     plt.title("Inverse Trajectories")
-    for i in range(num_demo):
+    for i in range(num_paired):
         plt.plot(times[i], inverse[i], color = "red")
-    plt.grid(alpha=0.3)
 
-    #plt.ylim(-0.3, 0.3)
+    for i in range(num_extra):
+        plt.plot(times[i], extra_inverse[i], color = "red")
+    
+    plt.grid(alpha=0.3)
     plt.show()
 
-    return X1, X2, Y1, Y2
+    return X1, X2, Y1, Y2, Y1_extra, Y2_extra, context_params_paired, context_params_extra
 
 def validate_model(means, stds, idx, demo_data, time_len, condition_points, epoch_count, d_y1, d_y2, forward, 
                    error_type='mse', plot=False):
